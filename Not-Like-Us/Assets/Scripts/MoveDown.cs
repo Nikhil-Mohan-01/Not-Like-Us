dc727ed5fb6a710610bbdd1bb72f6f256e104273
Nikhil-Mohan-01 <127566613+Nikhil-Mohan-01@users.noreply.github.com>
2024-08-20 10:58:45 -0400

    unity game updates 5

---
+++ Not-Like-Us/Assets/Scripts/MoveDown.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveDown : MonoBehaviour
+{
+    public float MoveTo;
+    public float speed;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y > MoveTo)
+        {
+            transform.position -= Vector3.up * speed * Time.deltaTime;
+        }
+    }
+}
